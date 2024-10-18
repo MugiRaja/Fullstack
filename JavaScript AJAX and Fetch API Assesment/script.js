4da9ce08f323efb67ad80ecbe936995e152518dc
MugiRaja <rmugi2463@gmail.com>
2024-10-18 18:41:17 +0530

    new

---
+++ JavaScript AJAX and Fetch API Assesment/script.js
@@ -0,0 +1,28 @@
+document.getElementById("jokebutton").addEventListener("click", function() {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const jokequestion = document.getElementById("jokequestion");
+        const jokeanswer = document.getElementById("jokeanswer");
+        jokequestion.innerText = data.setup;
+        jokeanswer.innerText = data.punchline;
+      })
+      .catch(error => console.log(error));
+  });
+
+
+
+
+//   document.getElementById("fetchJokeButton").addEventListener("click", function() {
+//     fetch("https://official-joke-api.appspot.com/random_joke")
+//       .then(response => response.json())
+//       .then(data => {
+//         console.log(data);
+//         const jokeSetup = document.getElementById("jokeSetup");
+//         const jokePunchline = document.getElementById("jokePunchline");
+//         jokeSetup.innerText = data.setup;
+//         jokePunchline.innerText = data.punchline;
+//       })
+//       .catch(error => console.error('Error:', error));
+//   });
