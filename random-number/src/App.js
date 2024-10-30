a7fc33527002d7506d2498ebcfa8e1fd88761d29
MugiRaja <rmugi2463@gmail.com>
2024-10-30 10:08:02 +0530

    new

--- random-number/src/App.js
+++ random-number/src/App.js
@@ -1,18 +1,19 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const ran = () =>{
-    return Math.random();
-    
-  };
+  const [randomNumber, setRandomNumber] = useState(0); 
+  const generateRandomNumber = () => { 
+    const number = Math.random(); 
+    setRandomNumber(number); };
   return (
 
     
 
     <div>
-<p>Random Number: {ran()}</p>
-<button variant="primary" onClick={ran}>Click me</button>
+<p>Random Number: {randomNumber}</p>
+<button variant="primary" onClick={generateRandomNumber}>Click me</button>
 
     </div>
 
