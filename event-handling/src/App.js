62f4875ef89da41e0f0529c57a36aaa81e3842ca
MugiRaja <rmugi2463@gmail.com>
2024-11-04 18:32:03 +0530

    new

--- event-handling/src/App.js
+++ event-handling/src/App.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { Stateandprops } from "./Pratice/Stateandprops";
-import ParentProps from "./Pratice/ParentProps";
-import ChildProps from "./Pratice/ChildProps";
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import State from "./Pratice/State";
+import ParentProps from "./Pratice/ParentProps";
+import { Stateandprops } from "./Pratice/Stateandprops";
 import UseEffect from "./Pratice/UseEffect";
 
 const App = () => {
   return (
-    <div>
-      <Stateandprops />
-      <ParentProps/>
-      <ChildProps/>
-      <State/>
-      <UseEffect/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h3>React Pratices</h3>
+        <ul>
+          <li>
+            <Link to="/state">01 State</Link>
+          </li>
+          <li>
+            <Link to="/parentProps">props</Link>
+          </li>
+          <li>
+            <Link to="/stateAndProps">State & Props</Link>
+          </li>
+          <li>
+            <Link to="/useEffect"> useeffect</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/state" element={<State />} />
+        <Route path="/parentProps" element={<ParentProps />} />
+        <Route path="/stateAndProps" element={<Stateandprops />} />
+        <Route path="/useEffect" element={<UseEffect />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
