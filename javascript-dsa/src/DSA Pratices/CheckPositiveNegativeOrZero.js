5b8cbf9614065fae840956cdfeba0be2fc5b78bc
MugiRaja <rmugi2463@gmail.com>
2024-11-07 10:09:15 +0530

    new

--- javascript-dsa/src/DSA Pratices/CheckPositiveNegativeOrZero.js
+++ javascript-dsa/src/DSA Pratices/CheckPositiveNegativeOrZero.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CheckPositiveNegativeOrZero = () => {
+  const [checkPositiveNegativeOrZero, setCheckPositiveNegativeOrZero] =
+    useState(0);
+    const [ansCheckPositiveNegativeOrZero,setAnsCheckPositiveNegativeOrZero] = () =>{
+        if ((Number(checkPositiveNegativeOrZero))> 0) {
+            ansCheckPositiveNegativeOrZero(checkPositiveNegativeOrZero+"is Positive")
+        }
+        else if ((Number(checkPositiveNegativeOrZero))< 0){
+           ansCheckPositiveNegativeOrZero(checkPositiveNegativeOrZero+"is Negative")
+        }
+        else{
+            alert((Number(checkPositiveNegativeOrZero))==0)
+        }
+    }
+  const getValue = (event) => {
+    setCheckPositiveNegativeOrZero(event.target.value);
+  };
   return (
-    <div>CheckPositiveNegativeOrZero</div>
-  )
-}
+    <div className="w-96 h-52 bg-orange-500 flex flex-col gap-5 items-center justify-center rounded-lg gap-2">
+      <input type="number" onChange={getValue} />
+    </div>
+  );
+};
 
-export default CheckPositiveNegativeOrZero
+export default CheckPositiveNegativeOrZero;
