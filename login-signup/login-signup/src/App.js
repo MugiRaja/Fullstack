31ab6fb321fc56df75ace510514d757542ec9bee
MugiRaja <rmugi2463@gmail.com>
2024-11-09 12:34:42 +0530

    new

--- login-signup/login-signup/src/App.js
+++ login-signup/login-signup/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginSignup from './Components/Assets/Login-Signup/LoginSignup';
 
 function App() {
   return (
     <div>
-      
+      <LoginSignup/>
     </div>
   );
 }
--- login-signup/login-signup/src/Components/Assets/Login-Signup/LoginSignup.js
+++ login-signup/login-signup/src/Components/Assets/Login-Signup/LoginSignup.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { FaRegUserCircle } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
-  )
-}
+    <div>
+      <div className="form-box">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaRegUserCircle />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <RiLockPasswordFill />
 
-export default LoginSignup
+          </div>
+          <div className="remember-forgot">
+            <label htmlFor=""><input type="checkbox" />Remember me</label>
+            <a href="#"Forgot Password></a>
+          </div>
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>Don't have an account?<a href="#">Register</a></p>
+          </div>
+        </form>
+      </div>
+      <div className="form-box">
+        <form action="">
+          <h1>Registration</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+            <FaRegUserCircle />
+          </div>
+          <div className="input-box">
+            <input type="email" placeholder="Email" required />
+            <FaRegUserCircle />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+            <RiLockPasswordFill />
+
+          </div>
+          <div className="remember-forgot">
+            <label htmlFor=""><input type="checkbox" />Remember me</label>
+            <a href="#"Forgot Password></a>
+          </div>
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>Already have an account?<a href="#">Login</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
