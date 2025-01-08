267ebf6f3cbf928d8acec0cfc95d7ab6ff5b7e16
MugiRaja <rmugi2463@gmail.com>
2025-01-08 17:28:51 +0530

    new

--- mern-auth/server/routes/authRotes.js
+++ mern-auth/server/routes/authRotes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { register, login, logout } from '../controllers/authController.js';
 
-
 const authRouter = express.Router();
 
 authRouter.post('/register', register);
