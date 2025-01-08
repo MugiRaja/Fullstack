d28a70a7816919f5ff349e46eaac8bce9b8f4cb3
MugiRaja <rmugi2463@gmail.com>
2025-01-08 17:29:12 +0530

    new

--- mern-auth/middleware/userAuth.js
+++ mern-auth/middleware/userAuth.js
@@ -1,25 +1,28 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-const userAuth = async(req, res, next) => {
-    const {token} = req.cookies;
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
 
-    if(!token) {
-        return res.json({success: false, message: 'Not Authorized. Login Again'});
+  if (!token) {
+    return res.json({ success: false, message: 'Not Authorized. Login Again' });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.body.userId = tokenDecode.id;
+    } else {
+      return res.json({
+        success: false,
+        message: 'Not Authorized. Login Again',
+      });
     }
 
-    try {
-        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-
-        if(tokenDecode.id) {
-            req.body.userId = tokenDecode.id;
-        } else {
-            return res.json({success: false, message: 'Not Authorized. Login Again'});
-        }
-
-        next();
-    } catch (error) {
-        return res.json({success: false, message: error.message});
-    }
-}
+    next();
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 export default userAuth;
